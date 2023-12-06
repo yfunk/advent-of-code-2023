67de68a4d302c84f52578b2f5d233852ce3e2525
yfunk <14955991+yfunk@users.noreply.github.com>
2023-12-06 01:12:56 +0100

    add day 5 solution

--- src/day-05/index.ts
+++ src/day-05/index.ts
@@ -1,35 +1,85 @@
 import { readInput } from 'io';
 import { parseGroups, splitString } from 'parse';
 import { chunk } from 'utils/array';
-import { isBetween, min, range, unique } from 'utils/math';
+import { isBetween, max, min, range, unique } from 'utils/math';
+import { asc } from 'utils/sort';
 
 type Map = { name: string; mappings: Mapping[] };
-type Mapping = { srcOffset: number; destOffset: number; length: number };
+type Mapping = { srcStart: number; destStart: number; length: number };
+
+type Range = { start: number; end: number };
 
 const input = await readInput('day-05');
 
 export const part1 = () => {
   const almanac = parseAlmanac(input);
 
-  const locations = findLocations(almanac.seeds, almanac.maps);
+  const locations: number[] = [];
+
+  for (const seed of almanac.seeds) {
+    let intermediate = seed;
+
+    for (const map of almanac.maps) {
+      let mapping = intermediate;
+
+      for (const { srcStart, destStart, length } of map.mappings) {
+        if (isBetween(intermediate, [srcStart, srcStart + length - 1])) {
+          mapping = intermediate - srcStart + destStart;
+          break;
+        }
+      }
+
+      intermediate = mapping;
+    }
+
+    locations.push(intermediate);
+  }
+
   const closest = min(locations);
 
   return closest;
 };
 
-// TODO: Brute-force approach only works on example, takes forever on actual input
 export const part2 = () => {
   const almanac = parseAlmanac(input);
 
-  const pairs = chunk(almanac.seeds, 2);
-  const seeds: number[] = [];
+  // ranges of seeds [start, end), last number excluded
+  let ranges = chunk(almanac.seeds, 2).map(
+    ([start, length]) => ({ start, end: start + length }) as Range
+  );
 
-  for (const [start, length] of pairs) {
-    seeds.push(...range(length, start));
+  // the closest location has to be at the start of one of the ranges
+  // map all seed ranges to location ranges and find the minimum
+  for (const map of almanac.maps) {
+    const mappedRanges: Range[] = [];
+
+    for (const range of ranges) {
+      let mapped = range;
+
+      for (const { srcStart, destStart, length } of map.mappings) {
+        const overlapStart = max([range.start, srcStart]);
+        const overlapEnd = min([range.end, srcStart + length]);
+
+        if (overlapStart < overlapEnd) {
+          mapped = {
+            start: overlapStart - srcStart + destStart,
+            end: overlapEnd - srcStart + destStart,
+          };
+
+          // check for remaining parts of range and add them back to `ranges` for mapping
+          if (overlapStart > range.start) ranges.push({ start: range.start, end: overlapStart });
+          if (range.end > overlapEnd) ranges.push({ start: overlapEnd, end: range.end });
+          break;
+        }
+      }
+
+      mappedRanges.push(mapped);
+    }
+
+    ranges = mappedRanges;
   }
 
-  const locations = findLocations(seeds, almanac.maps);
-  const closest = min(locations);
+  const closest = min(ranges.map(({ start }) => start));
 
   return closest;
 };
@@ -38,7 +88,7 @@ export const part2 = () => {
 const parseAlmanac = (input: string) => {
   const [[_seeds], ..._maps] = parseGroups(input);
 
-  const seeds = splitString(_seeds.split(':')[1], (v) => +v);
+  const seeds = splitString(_seeds.split(':')[1], (v) => Number(v));
   const maps: Map[] = [];
 
   for (const _map of _maps) {
@@ -48,8 +98,8 @@ const parseAlmanac = (input: string) => {
     const mappings: Mapping[] = [];
 
     for (const _mapping of _mappings) {
-      const [destOffset, srcOffset, length] = splitString(_mapping, (v) => +v);
-      mappings.push({ srcOffset, destOffset, length });
+      const [destStart, srcStart, length] = splitString(_mapping, (v) => Number(v));
+      mappings.push({ srcStart, destStart, length });
     }
 
     maps.push({ name, mappings });
@@ -57,33 +107,4 @@ const parseAlmanac = (input: string) => {
 
   return { seeds, maps };
 };
-
-const findLocations = (seeds: number[], maps: Map[]) => {
-  const locations: number[] = [];
-  let count = 1;
-
-  for (const seed of seeds) {
-    console.log(`seed: ${count++}/${seeds.length}`);
-    let intermediate = seed;
-
-    for (const map of maps) {
-      let mapping = intermediate;
-
-      for (const { srcOffset, destOffset, length } of map.mappings) {
-        if (isBetween(intermediate, [srcOffset, srcOffset + length - 1])) {
-          mapping = intermediate - srcOffset + destOffset;
-          break;
-        }
-      }
-
-      intermediate = mapping;
-    }
-
-    // found location after maps are processed
-    locations.push(intermediate);
-  }
-
-  return locations;
-};
-
 //#endregion
